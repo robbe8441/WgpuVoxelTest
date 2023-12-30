31715d381b51e188a10f7a5498e828109009be9f
robbe <robinjunker04@gmail.com>
2023-12-30 15:55:03 +0100

    Drawing-a-triangle

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,9 +1,48 @@
+use std::path::Path;
+use wgpu::util::DeviceExt;
 
 use winit::{
-    event::{Event, WindowEvent, self},
+    event::{Event, WindowEvent},
     event_loop::EventLoop,
-    window::{Window, self}, raw_window_handle::{WindowHandle, HasRawWindowHandle, HasDisplayHandle, DisplayHandle, HasWindowHandle},
+    window::{Icon, Window},
 };
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    const ATTRIBS: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
+
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        use std::mem;
+
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBS,
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [0.0, 0.5, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+];
 
 async fn run(event_loop: EventLoop<()>, window: Window) {
     let mut size = window.inner_size();
@@ -12,55 +51,67 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 
     let instance = wgpu::Instance::default();
 
-    let surface = unsafe {instance.create_surface(&window).unwrap()};
-    let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
-        power_preference : wgpu::PowerPreference::HighPerformance,
-        force_fallback_adapter : false,
-        compatible_surface : Some(&surface)
-    }).await.expect("Cant create adapter");
+    let surface = unsafe { instance.create_surface(&window).unwrap() };
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: Some(&surface),
+        })
+        .await
+        .expect("Cant create adapter");
 
-    let (device, queue) = adapter.request_device(
-        &wgpu::DeviceDescriptor {
-            label : Some("device"),
-            features : wgpu::Features::empty(),
-            limits : wgpu::Limits::downlevel_webgl2_defaults().using_resolution(adapter.limits())
-        }, None)
-    .await.expect("cant create device");
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: Some("device"),
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::downlevel_webgl2_defaults()
+                    .using_resolution(adapter.limits()),
+            },
+            None,
+        )
+        .await
+        .expect("cant create device");
 
     let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
 
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        label : Some("pipeline_layout"),
-        bind_group_layouts : &[],
+        label: Some("pipeline_layout"),
+        bind_group_layouts: &[],
         push_constant_ranges: &[],
     });
 
     let swapchain_capabilities = surface.get_capabilities(&adapter);
     let swapchain_format = swapchain_capabilities.formats[0];
 
-
+    let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Vertex Buffer"),
+        contents: bytemuck::cast_slice(VERTICES),
+        usage: wgpu::BufferUsages::VERTEX,
+    });
 
     let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label : Some("Render Pipeline"),
+        label: Some("Render Pipeline"),
         layout: Some(&pipeline_layout),
 
         vertex: wgpu::VertexState {
             module: &shader,
             entry_point: "vs_main",
-            buffers: &[]
+            buffers: &[Vertex::desc()],
         },
 
-        fragment : Some(wgpu::FragmentState { module: &shader, entry_point: "fs_main", 
-            targets: &[Some(swapchain_format.into())]
-
+        fragment: Some(wgpu::FragmentState {
+            module: &shader,
+            entry_point: "fs_main",
+            targets: &[Some(swapchain_format.into())],
         }),
 
         primitive: wgpu::PrimitiveState::default(),
         depth_stencil: None,
         multisample: wgpu::MultisampleState::default(),
-        multiview: None
+        multiview: None,
     });
-
 
     let mut config = wgpu::SurfaceConfiguration {
         usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
@@ -69,33 +120,95 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         height: size.height,
         present_mode: wgpu::PresentMode::Fifo,
         alpha_mode: swapchain_capabilities.alpha_modes[0],
-        view_formats: vec![]
+        view_formats: vec![],
     };
 
     surface.configure(&device, &config);
 
     let window = &window;
-    event_loop.run(move |event, target| {
-        let _ = (&instance, &adapter, &shader, &pipeline_layout);
+    event_loop
+        .run(move |event, target| {
+            let _ = (&instance, &adapter, &shader, &pipeline_layout);
 
-        if let Event::WindowEvent {
-            window_id : _,
-            event,
-        } = event {
-            match event {
-                WindowEvent::CloseRequested => target.exit(),
-                _ => {}
+            if let Event::WindowEvent {
+                window_id: _,
+                event,
+            } = event
+            {
+                match event {
+                    WindowEvent::CloseRequested => target.exit(),
+                    WindowEvent::Resized(physical_size) => {
+                        config.width = physical_size.width.max(1);
+                        config.height = physical_size.height.max(1);
+                        surface.configure(&device, &config);
+                        window.request_redraw();
+                    }
+
+                    WindowEvent::RedrawRequested => {
+                        let frame = surface
+                            .get_current_texture()
+                            .expect("Failed to get Current texture");
+                        let view = frame
+                            .texture
+                            .create_view(&wgpu::TextureViewDescriptor::default());
+
+                        let mut encoer =
+                            device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                                label: None,
+                            });
+                        {
+                            let mut render_pass =
+                                encoer.begin_render_pass(&wgpu::RenderPassDescriptor {
+                                    label: None,
+                                    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                                        view: &view,
+                                        resolve_target: None,
+                                        ops: wgpu::Operations {
+                                            load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
+                                            store: wgpu::StoreOp::Store,
+                                        },
+                                    })],
+
+                                    depth_stencil_attachment: None,
+                                    timestamp_writes: None,
+                                    occlusion_query_set: None,
+                                });
+                            render_pass.set_pipeline(&render_pipeline);
+                            render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+                            render_pass.draw(0..3, 0..1);
+                        }
+                        queue.submit(Some(encoer.finish()));
+                        frame.present();
+                    }
+                    _ => {}
+                }
             }
-        }
-
-    }).unwrap()
+        })
+        .unwrap()
 }
-
 
 pub async fn create_window() {
     let event_loop = EventLoop::new().unwrap();
-    let builder = winit::window::WindowBuilder::new();
+    let icon_path = concat!(env!("CARGO_MANIFEST_DIR"), "/../assets/Icon.png");
+    println!("{}", icon_path);
+
+    let builder = winit::window::WindowBuilder::new()
+        .with_title("Voxel Game Engine")
+        .with_window_icon(Some(load_icon(Path::new(icon_path))))
+        .with_theme(Some(winit::window::Theme::Dark));
     let window = builder.build(&event_loop).unwrap();
 
     run(event_loop, window).await;
+}
+
+fn load_icon(path: &Path) -> Icon {
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open(path)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
 }
